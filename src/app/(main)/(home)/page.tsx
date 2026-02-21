bb70191cf81f1e0f4d0a488e11fafff2e5b95b34
Tullysaurus <153677991+Tullysaurus@users.noreply.github.com>
2026-02-20 18:10:55 -0600

    Update page.tsx

--- src/app/(main)/(home)/page.tsx
+++ src/app/(main)/(home)/page.tsx
@@ -1,4 +1,13 @@
 export default function Home() {
+  /*I'm a highschool student who develops as a hobby.
+        I have a passion for working around rules that my school
+        puts into place. I develop cheats for the different softwares
+        that my school uses to monitor us, test us, and just assignments
+        in general. All of these projects are made in a way that allows
+        them to be hosted completely for free to allow anybody to use them
+        without money being a barrier, and to minimize the ability for
+        the hosting to be linked back to me.
+  */
   return (
     <div
       className="flex flex-col h-100vh w-100vh items-center pt-[10vh]"
