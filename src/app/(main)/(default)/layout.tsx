ba8a6f863025982b4948c6e65632c7ffbaf8c619
Tullysaurus <tullypro69@gmail.com>
2026-02-25 09:56:41 -0600

    Add hidden button to go to cheats page

--- src/app/(main)/(default)/layout.tsx
+++ src/app/(main)/(default)/layout.tsx
@@ -4,8 +4,10 @@ export default function DefaultLayout({
     children: React.ReactNode
 }){
     return (
-        <div className="w-full h-full">
+        <div className="w-full h-fit">
             {children}
+            <a href="/cheats" className="absolute right-0 bottom-0 w-[5vw] aspect-video h-fit bg-inherit cursor-pointer">
+            </a>
         </div>
     )
 }
