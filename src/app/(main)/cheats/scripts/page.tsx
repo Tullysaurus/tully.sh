fa776f0fb269be526e971458ea675af6496a29bd
Tullysaurus <tullypro69@gmail.com>
2026-02-27 11:27:00 -0600

    Fix scripts page

--- src/app/(main)/cheats/scripts/page.tsx
+++ src/app/(main)/cheats/scripts/page.tsx
@@ -12,14 +12,20 @@ interface Scripts {
 }
 
 export default async function ScriptsPage() {
-  const scripts: Scripts = await fetch("https://r2.tully.sh/scripts/scripts.json", {
+  const scripts: Scripts | null = await fetch("https://r2.tully.sh/scripts/scripts.json", {
     next: { revalidate: 60 } // Cache for 1 minute
-  }).then((res) => res.json());
+  })
+    .then((res) => res.json()).then((json) => json as Scripts)
+    .catch(() => (null));
 
   return (
     <div className="mx-auto flex min-h-screen w-full max-w-6xl flex-col items-center gap-8 pb-10 pt-10 lg:pt-16">
       <h1 className="px-4 text-center text-4xl italic font-light sm:text-5xl">tully.sh/cheats/<span className="text-[#FFC17B]">scripts</span></h1>
-      <ScriptsGrid scripts={scripts} />
+      {
+        scripts && (
+          <ScriptsGrid scripts={scripts} />
+        )
+      }
     </div>
   );
 }
