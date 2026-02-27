0cedd71190628cd35884a5339911b564c18f5a6b
Tullysaurus <tullypro69@gmail.com>
2026-02-27 16:48:47 -0600

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
         <head>
           <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon.ico" />
           <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+          <link rel="icon" type="image/x-icon" sizes="16x16" href="/icons/favicon-16x16.png" />
         </head>
       <body className={font.className}>
         {children}
