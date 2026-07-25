0913569cc240da11b41af16272e99d54a02515f2
Tullysaurus <153677991+Tullysaurus@users.noreply.github.com>
2026-07-25 15:58:57 +0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,11 @@ export default function RootLayout({
           <link rel="icon" type="image/png" sizes="16x16" href="https://tully.sh/icons/favicon-16x16.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="https://tully.sh/icons/favicon-32x32.png" />
           <link rel="icon" type="image/x-icon" sizes="16x16" href="https://tully.sh/icons/favicon.ico" />
-          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1717866889959310"
-     crossorigin="anonymous"></script>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1717866889959310"
+            crossOrigin="anonymous"
+          />
         </head>
       <body className={font.className}>
         {children}
